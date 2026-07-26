780fe62667a09a67bf658d43d862ba55ae049d0f
josephhall234 <joeyhall234@gmail.com>
2026-07-26 21:06:43 +0000

    outside-engineering_update_2

--- src/app/outside-engineering/page.tsx
+++ src/app/outside-engineering/page.tsx
@@ -3,17 +3,17 @@ import Image from "next/image";
 const interests = [
   {
     title: "Surfing",
-    copy: "I first learned to surf from my mother when I was 14 years old, and since then, it's been hard to keep me out of the ocean. At Cal Poly, I've been heavily involved with Mustang Surf Club, serving as treasurer for my final two years and leading surf lessons throughout all four. Surfing offers me a rare moment of peace, where I can enjoy both the serenity of looking back at the coastline and the thrill of riding a wave.",
+    copy: "I first learned to surf from my mother when I was 14 years old, and since then, it's been hard to keep me out of the ocean! At Cal Poly, I've been heavily involved with Mustang Surf Club, serving as treasurer for my final two years and leading surf lessons throughout all four. Surfing offers me a rare moment of peace, where I can enjoy both the serenity of looking back at the coastline and the thrill of riding a wave.",
     images: ["/images/outside-engineering/surfing.JPG"],
   },
   {
     title: "Backpacking",
-    copy: "In my opinion, there's no better way to see the world than to walk through it at a leisurely pace with a few friends. Visiting places like Point Reyes, Yosemite—pictured above—and Big Sur continually reinforces my love for nature and my desire to protect it.",
+    copy: "In my opinion, there's no better way to see the world than to walk through it at a leisurely pace with a few friends. Visiting places like Point Reyes, Yosemite (pictured above), and Big Sur continually reinforces my love for nature and my desire to protect it.",
     images: ["/images/outside-engineering/backpacking.jpg"],
   },
   {
     title: "Sourdough Making",
-    copy: "Although I've been making bread for nearly 10 years, I only began experimenting with sourdough about four years ago. Since then, I've kept—and killed—several starters. My current starter is more than three years old and is named Gary.",
+    copy: "Although I've been making bread for nearly 10 years, I only began experimenting with sourdough about four years ago. Since then, I've kept (and unfortunately killed) several starters. My current starter, named \"Gary\", is more than three years old and has served me well!",
     images: [
       "/images/outside-engineering/sourdough_1.jpeg",
       "/images/outside-engineering/sourdough_2.jpeg",
@@ -21,7 +21,7 @@ const interests = [
   },
   {
     title: "Home Brewing",
-    copy: "For many sourdough enthusiasts, the natural next step is home brewing beer. This is still a relatively new hobby for me, and I've only brewed a few batches so far. My two most recent attempts were all-grain versions of an English Pale Ale and a Belgian Witbier. The first turned out great; the second, not so much.",
+    copy: "For many sourdough enthusiasts, the natural next step is home brewing beer. This is still a relatively new hobby for me, and I've only brewed a few batches so far. My two most recent attempts were all-grain versions of an English Pale Ale and a Belgian Witbier. The first turned out great; the second, not so much!",
     images: [
       "/images/outside-engineering/homebrew_1.jpg",
       "/images/outside-engineering/homebrew_2.JPG",
